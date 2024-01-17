e77fae6db46f2bee03eae2276cccb470b53ff840
Carloslt <carloslt.proyectos@gmail.com>
2024-01-17 20:08:08 +0100

    add .env.template and supabase local dev

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -2,14 +2,16 @@ import { type BookID, type Book, type BookNotID } from '../types/book.type'
 import { createClient } from '@supabase/supabase-js'
 import { ModelError } from './error.t'
 
-const supabaseUrl = process.env.SUPABASE_URL ?? ''
-const supabaseKey = process.env.SUPABASE_KEY ?? ''
+const supabaseUrl = process.env.SUPABASE_URL ?? 'http://localhost:54321'
+const supabaseKey = process.env.SUPABASE_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 const db = createClient(supabaseUrl, supabaseKey)
 
 class BookModel {
   async getAll(): Promise<Book[]> {
-    const { data } = await db.from('books').select('*')
-    return data ?? []
+    const  result  = await db.from('books').select('*')
+  
+    console.log({result})
+    return result.data ?? []
   }
 
   async getById({ id }: BookID): Promise<Book[] | null> {
@@ -26,8 +28,9 @@ class BookModel {
   async createBook({ input }: { input: BookNotID }): Promise<Book[] | null> {
     const id = crypto.randomUUID()
     const newBook = { ...input, id }
-    const { data } = await db.from('books').insert([newBook]).select()
-    return data
+    const result = await db.from('books').insert([newBook]).select()
+    console.log({result})
+    return result.data
   }
 
   async deleteBook({ id }: BookID): Promise<boolean> {
