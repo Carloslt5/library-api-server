c46e1f1802ecab726a9c04659468a885660bc875
Carloslt <carloslt.proyectos@gmail.com>
2024-02-13 11:58:51 +0100

    ✅ task: add pg and connect bookmodel with db

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -1,55 +1,76 @@
 import { type Book, type BookID, type BookNotID } from '../schema/book.schema'
 import { ModelError } from '../error-handling/ModelError.type'
-import { createClient } from '@supabase/supabase-js'
+import { Pool } from 'pg'
 
-const supabaseUrl = process.env.SUPABASE_URL ?? 'http://localhost:54321'
-const supabaseKey =
-  process.env.SUPABASE_KEY ??
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
-export const db = createClient(supabaseUrl, supabaseKey)
+export const db = new Pool({
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  port: 5432,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+})
 
 class BookModel {
   async getAll(): Promise<Book[]> {
-    const result = await db.from('books').select()
-    if (result.error) {
-      throw new ModelError({ message: result.error.message, status: result.status })
+    try {
+      const query = 'SELECT * FROM books'
+      const result = await db.query(query)
+      return result.rows
+    } catch (error) {
+      throw new ModelError({ message: 'Can not found books', status: 400 })
     }
-    return result.data
   }
 
   async getById({ id }: { id: BookID }): Promise<Book[]> {
-    const result = await db.from('books').select().eq('id', id)
-    if (result.error) {
-      throw new ModelError({ message: result.error.message, status: result.status })
+    try {
+      const query = 'SELECT * FROM books WHERE id = $1'
+      const result = await db.query(query, [id])
+      return result.rows
+    } catch (error) {
+      throw new ModelError({ message: 'Can not found this book', status: 400 })
     }
-    return result.data
   }
 
   async createBook({ input }: { input: BookNotID }): Promise<boolean> {
     const id: BookID = crypto.randomUUID()
-    const newBook: Book = { id, ...input }
-    const result = await db.from('books').insert(newBook)
-    if (result.error) {
-      throw new ModelError({ message: result.error.message, status: result.status })
+    const { title, author, categories, link, year, imageURL } = input
+    try {
+      const query = `
+      INSERT INTO books (id, title, author, categories, link, year, "imageURL")
+      VALUES ($1, $2, $3, $4, $5, $6, $7)`
+      const values = [id, title, author, categories, link, year, imageURL]
+      await db.query(query, values)
+      return true
+    } catch (error) {
+      throw new ModelError({ message: 'Can not created book', status: 400 })
     }
-    return true
   }
 
   async updateBook({ id, input }: { id: BookID; input: BookNotID }): Promise<boolean> {
-    const result = await db.from('books').update(input).eq('id', id)
-    if (result.error) {
-      throw new ModelError({ message: result.error.message, status: result.status })
+    const { title, author, categories, link, year, imageURL } = input
+    try {
+      const query = `
+        UPDATE books
+        SET title = $1, author = $2, categories = $3, link = $4, year = $5, "imageURL" = $6
+        WHERE id = $7 
+      `
+      const values = [title, author, categories, link, year, imageURL, id]
+      await db.query(query, values)
+      return true
+    } catch (error) {
+      throw new ModelError({ message: 'Can not update book', status: 400 })
     }
-    return true
   }
 
   async deleteBook({ id }: { id: BookID }): Promise<boolean> {
-    const result = await db.from('books').delete().eq('id', id)
-
-    if (result.error) {
-      throw new ModelError({ message: result.error.message, status: result.status })
+    try {
+      const query = 'DELETE FROM books WHERE id = $1'
+      const values = [id]
+      await db.query(query, values)
+      return true
+    } catch (error) {
+      throw new ModelError({ message: 'Can not delete book', status: 400 })
     }
-    return true
   }
 }
 
