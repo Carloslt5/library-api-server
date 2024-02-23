f42c3619d8195a3420681f7e91a57bb6633f1c4a
Carloslt <carloslt.proyectos@gmail.com>
2024-02-23 14:54:57 +0100

    🗑 delete string conexion

--- api/src/controllers/upload.controllers.ts
+++ api/src/controllers/upload.controllers.ts
@@ -3,10 +3,8 @@ import fs from 'fs/promises'
 import { ModelError } from '../error-handling/ModelError.type'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL ?? 'http://localhost:54321'
-const supabaseKey =
-  process.env.SUPABASE_KEY ??
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
+const supabaseUrl = process.env.SUPABASE_URL ?? ''
+const supabaseKey = process.env.SUPABASE_KEY ?? ''
 export const db = createClient(supabaseUrl, supabaseKey)
 
 export const uploadFile: RequestHandler = async (req, res, next) => {
